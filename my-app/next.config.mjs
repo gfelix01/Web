7f3252e1ce8070343dea0e58f0e9720328a4e848
gfelix01 <gabriel_arturo01@hotmail.com>
2025-03-14 09:52:09 -0400

    remove swcMinify option from Next.js configuration

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Ayuda a detectar problemas en desarrollo
-  swcMinify: true, // Optimiza el código para mejor rendimiento
   output: 'standalone', // Recomendado para despliegues en Vercel y Docker
 };
 
