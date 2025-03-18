15ddefee9045da443054886fcae6b9f498112625
gfelix01 <gabriel_arturo01@hotmail.com>
2025-03-18 15:08:05 -0400

    update next.js version to 13.4.19 and adjust package.json for compatibility; modify build script and retain image format optimization in next.config.mjs

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'], // Mantén esto si quieres optimización para estos formatos
+    formats: ['image/avif', 'image/webp'], // Keep this if you want optimization for these formats
   },
   reactStrictMode: true,
 };
